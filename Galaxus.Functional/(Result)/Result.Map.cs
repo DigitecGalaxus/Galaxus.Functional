8c6dd93c1f6dad92922c1bc769d67afdf8935544
Philipp Kiener <kiener.phil@gmail.com>
2020-06-20 20:23:38 +0200

    Extract regions into feature files and make Result partial

---
+++ Galaxus.Functional/(Result)/Result.Map.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Threading.Tasks;
+
+namespace Galaxus.Functional
+{
+    public sealed partial class Result<TOk, TErr>
+    {
+        /// <summary>
+        /// Maps a <see cref="Result{TOk, TErr}"/> to <see cref="Result{TOkTo, TErr}"/> by applying a function to a contained <b>Ok</b> value,
+        /// leaving an <b>Err</b> value untouched. This function can be used to compose the results of two functions.
+        /// </summary>
+        /// <typeparam name="TOkTo">The type to map <b>Ok</b> to.</typeparam>
+        /// <param name="map">The mapping function.</param>
+        public Result<TOkTo, TErr> Map<TOkTo>(Func<TOk, TOkTo> map)
+        {
+            return Match(
+                ok =>
+                {
+                    if (map == null)
+                        throw new ArgumentNullException(nameof(map));
+
+                    return map(ok);
+                },
+                err => err.ToErr<TOkTo, TErr>()
+            );
+        }
+
+        /// <summary>
+        /// Maps a <see cref="Result{TOk, TErr}"/> to <see cref="Result{TOkTo, TErr}"/> by applying a function to a contained <b>Ok</b> value,
+        /// leaving an <b>Err</b> value untouched. This function can be used to compose the results of two functions.
+        /// </summary>
+        /// <typeparam name="TOkTo">The type to map <b>Ok</b> to.</typeparam>
+        /// <param name="continuation">The mapping function.</param>
+        public Task<Result<TOkTo, TErr>> MapAsync<TOkTo>(Func<TOk, Task<TOkTo>> continuation)
+        {
+            return MatchAsync(
+                async ok => new Result<TOkTo, TErr>(await continuation(ok)),
+                err => err.ToErr<TOkTo, TErr>());
+        }
+
+        /// <summary>
+        /// Maps a <see cref="Result{TOk, TErr}"/> to <see cref="Result{TOk, TErrTo}"/> by applying a function to a contained <b>Err</b> value,
+        /// leaving an <b>Ok</b> value untouched. This function can be used to compose the results of two functions.
+        /// </summary>
+        /// <typeparam name="TErrTo">The type to map "Err" to.</typeparam>
+        /// <param name="map">The mapping function.</param>
+        public Result<TOk, TErrTo> MapErr<TErrTo>(Func<TErr, TErrTo> map)
+        {
+            return Match(
+                ok => ok.ToOk<TOk, TErrTo>(),
+                err =>
+                {
+                    if (map == null)
+                        throw new ArgumentNullException(nameof(map));
+
+                    return map(err);
+                }
+            );
+        }
+
+        /// <summary>
+        /// Maps a <see cref="Result{TOk, TErr}"/> to <see cref="Result{TOk, TErrTo}"/> by applying a function to a contained <b>Err</b> value,
+        /// leaving an <b>Ok</b> value untouched. This function can be used to compose the results of two functions.
+        /// </summary>
+        /// <typeparam name="TErrTo">The type to map "Err" to.</typeparam>
+        /// <param name="continuation">The mapping function.</param>
+        public Task<Result<TOk, TErrTo>> MapErrAsync<TErrTo>(Func<TErr, Task<TErrTo>> continuation)
+        {
+            return MatchAsync(
+                ok => ok.ToOk<TOk, TErrTo>(),
+                async err => new Result<TOk, TErrTo>(await continuation(err)));
+        }
+    }
+}
---
+++ Galaxus.Functional/(Result)/Result.Match.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Threading.Tasks;
+
+namespace Galaxus.Functional
+{
+    public sealed partial class Result<TOk, TErr>
+    {
+        /// <summary>
+        /// Provides access to <b>self</b>'s content by calling <paramref name="onOk"/> and passing in <b>Ok</b>
+        /// or calling <paramref name="onErr"/> and passing in <b>Err</b>.
+        /// </summary>
+        /// <param name="onOk">Called when <b>self</b> contains <b>Ok</b>. The argument to this action is never the <b>null</b> reference.</param>
+        /// <param name="onErr">Called when <b>self</b> contains <b>Err</b>. The argument to this action is never the <b>null</b> reference.</param>
+        public void Match(Action<TOk> onOk, Action<TErr> onErr)
+        {
+            if (IsOk)
+            {
+                if (onOk is null)
+                    throw new ArgumentNullException(nameof(onOk));
+
+                onOk(_ok);
+            }
+            else
+            {
+                if (onErr is null)
+                    throw new ArgumentNullException(nameof(onErr));
+
+                onErr(_err);
+            }
+        }
+
+        /// <summary>
+        /// Provides access to <b>self</b>'s content by calling <paramref name="onOk"/> and passing in <b>Ok</b>
+        /// or calling <paramref name="onErr"/> and passing in <b>Err</b>.
+        /// </summary>
+        /// <param name="onOk">Called when <b>self</b> contains <b>Ok</b>. The argument to this function is never the <b>null</b> reference.</param>
+        /// <param name="onErr">Called when <b>self</b> contains <b>Err</b>. The argument to this function is never the <b>null</b> reference.</param>
+        public T Match<T>(Func<TOk, T> onOk, Func<TErr, T> onErr)
+        {
+            if (IsOk)
+            {
+                if (onOk is null)
+                    throw new ArgumentNullException(nameof(onOk));
+
+                return onOk(_ok);
+            }
+
+            if (onErr is null)
+                throw new ArgumentNullException(nameof(onErr));
+
+            return onErr(_err);
+        }
+
+        public Task<TResult> MatchAsync<TResult>(Func<TOk, Task<TResult>> onOk, Func<TErr, Task<TResult>> onErr)
+        {
+            return Match(
+                async ok => await onOk(ok),
+                async err => await onErr(err));
+        }
+
+        public Task<TResult> MatchAsync<TResult>(Func<TOk, Task<TResult>> onOk, Func<TErr, TResult> onErr)
+        {
+            return Match(
+                async ok => await onOk(ok),
+                err => Task.FromResult(onErr(err)));
+        }
+
+        public Task<TResult> MatchAsync<TResult>(Func<TOk, TResult> onOk, Func<TErr, Task<TResult>> onErr)
+        {
+            return Match(
+                ok => Task.FromResult(onOk(ok)),
+                async err => await onErr(err));
+        }
+    }
+}
