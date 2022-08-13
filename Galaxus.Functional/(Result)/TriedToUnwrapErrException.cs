aa8d071765f6c39ef5475c40c3f052ee5b4b0432
Philipp Kiener <kiener.phil@gmail.com>
2022-08-13 14:15:17 +0200

    Seal the result-exceptino as well

--- Galaxus.Functional/(Result)/TriedToUnwrapErrException.cs
+++ Galaxus.Functional/(Result)/TriedToUnwrapErrException.cs
@@ -5,7 +5,7 @@ namespace Galaxus.Functional
     /// <summary>
     ///     Thrown when an attempt was made to unwrap a <see cref="Result{TOk, TErr}" /> containing <b>Err</b>.
     /// </summary>
-    public class TriedToUnwrapErrException : Exception
+    public sealed class TriedToUnwrapErrException : Exception
     {
         /// <summary>
         ///     Create an <see cref="TriedToUnwrapErrException" /> object.
