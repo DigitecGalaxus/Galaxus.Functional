a4a6371e4dcb0f6f3dedd829f42615b50cb80673
Nightsir <christian.sami96@gmail.com>
2023-08-11 14:06:49 +0200

    R - make asyncoptionextensions partion for more consitent naming

--- Galaxus.Functional/(Option)/(Async)/AsyncOptionExtensions.Map.cs
+++ Galaxus.Functional/(Option)/(Async)/AsyncOptionExtensions.Map.cs
@@ -6,7 +6,7 @@ namespace Galaxus.Functional;
 /// <summary>
 ///     Extensions to map operations for <see cref="Option{T}" /> using async methods or <see cref="Task" />s.
 /// </summary>
-public static class AsyncOptionExtensions_Map
+public static partial class AsyncOptionExtensions
 {
     /// <inheritdoc cref="Option{T}.MapOrElse{TTo}" />
     public static async Task<TTo> MapOrElseAsync<T, TTo>(this Task<Option<T>> self, Func<T, TTo> map, Func<TTo> fallback)
