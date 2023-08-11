5f6f6797f5000c58706fc7ee043e413f74aedf4c
Nightsir <christian.sami96@gmail.com>
2023-08-11 13:02:48 +0200

    F!! - Add MapOrElseAsync both functionarguments test

---
+++ Galaxus.Functional.Tests/Option/Async/AsyncOptionExtensions.MapOrElseAsyncTest.cs
@@ -0,0 +1,60 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+using static Galaxus.Functional.Tests.Option.Async.OptionFactory;
+
+namespace Galaxus.Functional.Tests.Option.Async;
+
+[TestFixture]
+internal class MapOrElseAsyncTest
+{
+    public sealed class BothFunctionArguments : MapOrElseAsyncTest
+    {
+        [Test]
+        public async Task CallsMap_WhenSelfIsSome()
+        {
+            var mapInvoked = false;
+            var fallbackInvoked = false;
+
+            var value = await CreateSomeTask("value").MapOrElseAsync(
+                s =>
+                {
+                    mapInvoked = true;
+                    return s.Length.ToOption();
+                },
+                () =>
+                {
+                    fallbackInvoked = true;
+                    return 2.ToOption();
+                });
+
+            Assert.AreEqual(5.ToOption(), value);
+            Assert.IsTrue(mapInvoked);
+            Assert.IsFalse(fallbackInvoked);
+        }
+
+        [Test]
+        public async Task CallsFallback_WhenSelfIsNone()
+        {
+            var value = await CreateNoneTask().UnwrapOrElseAsync(() => "failed");
+            Assert.AreEqual("failed", value);
+
+            var mapInvoked = false;
+            var fallbackInvoked = false;
+
+            Assert.AreEqual(42.ToOption(), await CreateNoneTask().MapOrElseAsync(
+                s =>
+                {
+                    mapInvoked = true;
+                    return s.Length.ToOption();
+                },
+                () =>
+                {
+                    fallbackInvoked = true;
+                    return 42.ToOption();
+                }));
+
+            Assert.IsFalse(mapInvoked);
+            Assert.IsTrue(fallbackInvoked);
+        }
+    }
+}
---
+++ Galaxus.Functional/(Option)/(Async)/AsyncOptionExtensions.Map.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Galaxus.Functional;
+
+/// <summary>
+///     Extensions to map operations for <see cref="Option{T}" /> using async methods or <see cref="Task" />s.
+/// </summary>
+public static class AsyncOptionExtensions_Map
+{
+    /// <inheritdoc cref="Option{T}.MapOrElse{TTo}" />
+    public static Task<TTo> MapOrElseAsync<T, TTo>(this Task<Option<T>> self, Func<T, TTo> map, Func<TTo> fallback)
+    {
+        throw new NotImplementedException();
+    }
+}
