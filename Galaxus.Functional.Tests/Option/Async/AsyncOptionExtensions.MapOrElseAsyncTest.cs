92a052f58e503d4747b92395bb632ff43493111c
Nightsir <christian.sami96@gmail.com>
2023-08-11 13:17:19 +0200

    F!! - add syncoption maporelseasync async map test

--- Galaxus.Functional.Tests/Option/Async/AsyncOptionExtensions.MapOrElseAsyncTest.cs
+++ Galaxus.Functional.Tests/Option/Async/AsyncOptionExtensions.MapOrElseAsyncTest.cs
@@ -198,4 +198,52 @@ internal class MapOrElseAsyncTest
             Assert.IsTrue(fallbackInvoked);
         }
     }
+
+    public sealed class SyncOption_AsyncMapArguments : MapOrElseAsyncTest
+    {
+        [Test]
+        public async Task CallsMap_WhenSelfIsSome()
+        {
+            var mapInvoked = false;
+            var fallbackInvoked = false;
+
+            var value = await CreateSome("value").MapOrElseAsync(
+                s =>
+                {
+                    mapInvoked = true;
+                    return Task.FromResult(s.Length.ToOption());
+                },
+                () =>
+                {
+                    fallbackInvoked = true;
+                    return 2.ToOption();
+                });
+
+            Assert.AreEqual(5.ToOption(), value);
+            Assert.IsTrue(mapInvoked);
+            Assert.IsFalse(fallbackInvoked);
+        }
+
+        [Test]
+        public async Task CallsFallback_WhenSelfIsNone()
+        {
+            var mapInvoked = false;
+            var fallbackInvoked = false;
+
+            Assert.AreEqual(42.ToOption(), await Option<string>.None.MapOrElseAsync(
+                s =>
+                {
+                    mapInvoked = true;
+                    return Task.FromResult(s.Length.ToOption());
+                },
+                () =>
+                {
+                    fallbackInvoked = true;
+                    return 42.ToOption();
+                }));
+
+            Assert.IsFalse(mapInvoked);
+            Assert.IsTrue(fallbackInvoked);
+        }
+    }
 }
--- Galaxus.Functional/(Option)/(Async)/AsyncOptionExtensions.Map.cs
+++ Galaxus.Functional/(Option)/(Async)/AsyncOptionExtensions.Map.cs
@@ -31,4 +31,10 @@ public static class AsyncOptionExtensions_Map
     {
         throw new NotImplementedException();
     }
+
+    /// <inheritdoc cref="Option{T}.MapOrElse{TTo}" />
+    public static Task<TTo> MapOrElseAsync<T, TTo>(this Option<T> self, Func<T, Task<TTo>> map, Func<TTo> fallback)
+    {
+        throw new NotImplementedException();
+    }
 }
