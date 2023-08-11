09bf00310578a4a022ec0308ef7c3991e66b35e9
Nightsir <christian.sami96@gmail.com>
2023-08-11 13:18:37 +0200

    F!! - add syncoption maporelseasync async fallback test

--- Galaxus.Functional/(Option)/(Async)/AsyncOptionExtensions.Map.cs
+++ Galaxus.Functional/(Option)/(Async)/AsyncOptionExtensions.Map.cs
@@ -37,4 +37,10 @@ public static class AsyncOptionExtensions_Map
     {
         throw new NotImplementedException();
     }
+
+    /// <inheritdoc cref="Option{T}.MapOrElse{TTo}" />
+    public static Task<TTo> MapOrElseAsync<T, TTo>(this Option<T> self, Func<T, TTo> map, Func<Task<TTo>> fallback)
+    {
+        throw new NotImplementedException();
+    }
 }
