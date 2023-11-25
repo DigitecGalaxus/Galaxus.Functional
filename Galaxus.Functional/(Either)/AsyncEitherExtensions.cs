a0f992e2993b513e7204611ec48f1ff9b338e9cd
Philipp Kiener <kiener.phil@gmail.com>
2023-11-25 13:54:55 +0100

    Add ConfigureAwait() to all awaits
    But without formatting :>

--- Galaxus.Functional/(Either)/AsyncEitherExtensions.cs
+++ Galaxus.Functional/(Either)/AsyncEitherExtensions.cs
@@ -11,60 +11,60 @@ public static class AsyncEitherExtensions
     /// <inheritdoc cref="Either{A,B}.Match" />
     public static async Task<T> MatchAsync<A, B, T>(this Either<A, B> self, Func<A, Task<T>> onA, Func<B, Task<T>> onB)
     {
-        return await self.Match(onA, onB);
+        return await self.Match(onA, onB).ConfigureAwait(false);
     }
 
     /// <inheritdoc cref="Either{A,B}.Match" />
     public static async Task<T> MatchAsync<A, B, T>(this Either<A, B> self, Func<A, Task<T>> onA, Func<B, T> onB)
     {
-        return await self.Match(onA, b => Task.FromResult(onB(b)));
+        return await self.Match(onA, b => Task.FromResult(onB(b))).ConfigureAwait(false);
     }
 
     /// <inheritdoc cref="Either{A,B}.Match" />
     public static async Task<T> MatchAsync<A, B, T>(this Either<A, B> self, Func<A, T> onA, Func<B, Task<T>> onB)
     {
-        return await self.Match(a => Task.FromResult(onA(a)), onB);
+        return await self.Match(a => Task.FromResult(onA(a)), onB).ConfigureAwait(false);
     }
 
     /// <inheritdoc cref="Either{A,B,C}.Match" />
     public static async Task<T> MatchAsync<A, B, C, T>(this Either<A, B, C> self, Func<A, Task<T>> onA, Func<B, Task<T>> onB, Func<C, Task<T>> onC)
     {
-        return await self.Match(onA, onB, onC);
+        return await self.Match(onA, onB, onC).ConfigureAwait(false);
     }
 
     /// <inheritdoc cref="Either{A,B,C}.Match" />
     public static async Task<T> MatchAsync<A, B, C, T>(this Either<A, B, C> self, Func<A, Task<T>> onA, Func<B, Task<T>> onB, Func<C, T> onC)
     {
-        return await self.Match(onA, onB, c => Task.FromResult(onC(c)));
+        return await self.Match(onA, onB, c => Task.FromResult(onC(c))).ConfigureAwait(false);
     }
 
     /// <inheritdoc cref="Either{A,B,C}.Match" />
     public static async Task<T> MatchAsync<A, B, C, T>(this Either<A, B, C> self, Func<A, Task<T>> onA, Func<B, T> onB, Func<C, Task<T>> onC)
     {
-        return await self.Match(onA, b => Task.FromResult(onB(b)), onC);
+        return await self.Match(onA, b => Task.FromResult(onB(b)), onC).ConfigureAwait(false);
     }
 
     /// <inheritdoc cref="Either{A,B,C}.Match" />
     public static async Task<T> MatchAsync<A, B, C, T>(this Either<A, B, C> self, Func<A, Task<T>> onA, Func<B, T> onB, Func<C, T> onC)
     {
-        return await self.Match(onA, b => Task.FromResult(onB(b)), c => Task.FromResult(onC(c)));
+        return await self.Match(onA, b => Task.FromResult(onB(b)), c => Task.FromResult(onC(c))).ConfigureAwait(false);
     }
 
     /// <inheritdoc cref="Either{A,B,C}.Match" />
     public static async Task<T> MatchAsync<A, B, C, T>(this Either<A, B, C> self, Func<A, T> onA, Func<B, Task<T>> onB, Func<C, Task<T>> onC)
     {
-        return await self.Match(a => Task.FromResult(onA(a)), onB, onC);
+        return await self.Match(a => Task.FromResult(onA(a)), onB, onC).ConfigureAwait(false);
     }
 
     /// <inheritdoc cref="Either{A,B,C}.Match" />
     public static async Task<T> MatchAsync<A, B, C, T>(this Either<A, B, C> self, Func<A, T> onA, Func<B, Task<T>> onB, Func<C, T> onC)
     {
-        return await self.Match(a => Task.FromResult(onA(a)), onB, c => Task.FromResult(onC(c)));
+        return await self.Match(a => Task.FromResult(onA(a)), onB, c => Task.FromResult(onC(c))).ConfigureAwait(false);
     }
 
     /// <inheritdoc cref="Either{A,B,C}.Match" />
     public static async Task<T> MatchAsync<A, B, C, T>(this Either<A, B, C> self, Func<A, T> onA, Func<B, T> onB, Func<C, Task<T>> onC)
     {
-        return await self.Match(a => Task.FromResult(onA(a)), b => Task.FromResult(onB(b)), onC);
+        return await self.Match(a => Task.FromResult(onA(a)), b => Task.FromResult(onB(b)), onC).ConfigureAwait(false);
     }
 }
